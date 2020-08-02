b59971c86317252e28e5ae6e3bed25bda20470b0
adesyudhatama <adesyudhatama@gmail.com>
2020-08-02 15:14:56 +0700

    text field

--- lib/19_textfield_widget.dart
+++ lib/19_textfield_widget.dart
@@ -10,8 +10,33 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+  TextEditingController controller = TextEditingController();
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          leading: Icon(Icons.home),
+          title: Text("Text Field"),
+        ),
+        body: Center(
+          child: Container(
+            margin: EdgeInsets.all(10),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: <Widget>[
+                TextField(
+                  onChanged: (value) {
+                    setState(() {});
+                  },
+                  controller: controller,
+                ),
+                Text(controller.text),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
